ca0576cd85d997936448238e30a591dae93b4261
Janitha Tennakoon <janitha000@gmail.com>
2020-08-31 14:26:00 +0800

    Added header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBqOiBTaqifP71VqBbgUPy4g47PTFQyv34",
+    authDomain: "facebook-react-clone.firebaseapp.com",
+    databaseURL: "https://facebook-react-clone.firebaseio.com",
+    projectId: "facebook-react-clone",
+    storageBucket: "facebook-react-clone.appspot.com",
+    messagingSenderId: "243007179720",
+    appId: "1:243007179720:web:fb8532c6fdc7cd2293cefd",
+    measurementId: "G-207X4EN9DR"
+};
